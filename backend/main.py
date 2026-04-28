dfdca240c6246466b9b9c3efde7a6e373b0ec1fc
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2026-04-28 17:21:28 -0400

    added notepeel.net to cors

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,7 @@ app = FastAPI(
 # CORS middleware
 cors_origins = [
     "https://notepeelfrontend.onrender.com",
+    "https://notepeel.net",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
